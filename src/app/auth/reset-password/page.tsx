1a5268698ce85b5338bf2a27dfc3d23d11969267
jayll0 <aryawijaya6664@gmail.com>
2025-09-27 17:13:35 +0700

    fix resetpw

--- src/app/auth/reset-password/page.tsx
+++ src/app/auth/reset-password/page.tsx
@@ -1,10 +1,15 @@
-
 import { Suspense } from "react";
 import ResetPWContent from "@/components/auth/ResetPW/ResetPWContent";
 
 export default function Page() {
   return (
-    <Suspense fallback={<div className="flex justify-center items-center h-screen">Loading...</div>}>
+    <Suspense
+      fallback={
+        <div className="flex justify-center items-center h-screen">
+          Loading...
+        </div>
+      }
+    >
       <ResetPWContent />
     </Suspense>
   );
--- src/app/components/auth/ResetPW/ResetPWContent.tsx
+++ src/app/components/auth/ResetPW/ResetPWContent.tsx
@@ -28,11 +28,14 @@ export default function ResetPWContent() {
 
     setLoading(true);
     try {
-      const res = await fetch("http://localhost:3000/auth/reset-password", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ email, otp, password }),
-      });
+      const res = await fetch(
+        `${process.env.NEXT_PUBLIC_API_URL}/auth/reset-password`,
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({ email, otp, password }),
+        }
+      );
 
       const data = await res.json();
       if (data.success) {
